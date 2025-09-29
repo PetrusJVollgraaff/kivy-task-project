f1a813d2530f635736de19ebabbc6caba51c9cca
PetrusJVollgraaff <petrusvollgraaff1988@hotmail.co.za>
2025-09-29 16:59:22 +0200

    editor sceen start

--- src/modules/editorscreen/screen.py
+++ src/modules/editorscreen/screen.py
@@ -5,9 +5,13 @@ class EditorScreen(MDScreen, GlobalElements):
         super().__init__(**kw)
         self.name="editor"
 
+        self.top_bar = self.topBar(title="Create ToDo", left_action_items=[["arrow-left", lambda x: self.home_page()]])        
         layout = MDBoxLayout(orientation='vertical')
-        layout.add_widget(self.topBar(title="Create ToDo"))
+        layout.add_widget(self.top_bar)
 
         self.add_widget(layout)
 
+    def home_page(self):
+        print("to home")
+        self.manager.current = "home"
     
